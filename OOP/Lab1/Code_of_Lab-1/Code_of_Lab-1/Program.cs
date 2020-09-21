6290e96b49f5d3cdb09a7eab4eb006018c0ce31d
Rifat <rifat@MBP-Rifat.sknt.ru>
2020-09-21 15:51:47 +0300

    ...

---
+++ OOP/Lab1/Code_of_Lab-1/Code_of_Lab-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Code_of_Lab_1
+{
+    class Cars
+    {
+        private int wheels;
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test Test Code!!");
+            int cntu = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= cntu; i++) {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("Number " + i + "is even.");
+                } else
+                {
+                    Console.WriteLine("Number " + i + "is odd");
+                }
+                
+            }
+            double num1 = 11.7, num2 = 23.6;
+            Console.WriteLine(num1 + " + " + num2 + " = " + (num1 + num2));
+
+        }
+    }
+}
