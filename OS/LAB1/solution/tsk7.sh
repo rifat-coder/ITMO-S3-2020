6bc37e47290fa1f7cc54686f8055714e87ad623a
Rifat_cnt8 <rifat0531m@gmail.com>
2020-09-23 16:30:32 +0300

    Made task 7 in lab1

--- OS/LAB1/solution/tsk7.sh
+++ OS/LAB1/solution/tsk7.sh
@@ -1,2 +1,2 @@
-#!/bin/bash/
-
+#!/bin/bash
+egrep -i -o -s --binary-files=without-match -h "[a-z0-9.-_]+@[a-z0-9.-_]+\.[a-z]+" -r /etc/ | uniq | awk '{ printf("%s, ", $1 ) }' | sed 's/..$//' > emails.lst
