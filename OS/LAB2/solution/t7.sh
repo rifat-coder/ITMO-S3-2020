fc4fe41f1eeb53bce8215898ff7d9cd4d64294c2
CentOS-8 Rifat <rifat0531m@gmail.com>
2020-10-16 18:35:04 +0300

    F

--- OS/LAB2/solution/t7.sh
+++ OS/LAB2/solution/t7.sh
@@ -1,29 +1,22 @@
-#!/bin/bash
-for pid in $(ps -A -o pid —no-header) 
-do
-file=/proc/$pid/io
-if [[ -f $file ]]
-then
-read_bytes=$(grep read_bytes $file | grep -E -o '[[:digit:]]+')
-echo $pid $read_bytes » temp
-fi
-done
-sudo rm answer7
-cat temp
-echo "Sleeping for 60s"
-sleep 60
-echo "Completed"
-while read string 
+#!/bin/bash 
+(for i in $(ls /proc | grep "[0-9]") 
 do 
-pid=$(echo $string | awk '{print $1}')
-old_read_bytes=$(echo $string | awk '{print $2}')
-file=/proc/$pid/io
-if [[ -f $file ]] 
-then
-new_read_bytes=$(grep read_bytes $file | grep -E -o '[[:digit:]]+')
-echo $pid $(echo $new_read_bytes $old_read_bytes | awk '{print $1-$2}') » kek
-fi
-done < temp
-sudo sort -n -k2 kek > 7at.sh
-sudo rm temp
-sudo rm kek
+awk '$1 == "rchar:" {print "%d ", $2}' /proc/$i/io » term1.info 
+cmd=$(ps -e -o pid,cmd | awk -v id=$i '$1 == id {print $2}') 
+echo $i $cmd » term1.info 
+done 
+sleep 60 
+for i in $(ls /proc | grep "[0-9]") 
+do 
+awk '$1 == "rchar:" {print "%d ", $2}' /proc/$i/io » term2.info 
+echo $i » term2.info 
+done 
+while read str 
+do 
+pid=$(echo $str | awk '{print $2}') 
+mem=$(echo $str | awk '{print $1}') 
+awk -v id=$pid -v m=$mem '{if ($2 == id) {printf "PID %d : CMD %s : Memory %d ", $2, $3, m-$1; print "\n"}}' term1.info » term3.info 
+done < term2.info 
+sort -n -k 8 | tail -n 3 | sort -n -k 8 
+rm *.info 
+) 2> /dev/null
