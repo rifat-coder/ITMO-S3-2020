5ac2dd35881cf31fce5b4ae870ea0364a2cfcdd0
Rifat_cnt8 <rifat0531m@gmail.com>
2020-12-23 15:16:44 +0300

    update

---
+++ OS/LAB5/m3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo $$ > .pid3
+declare -a array
+declare -a numbers=(1,2,3,4,5,6,7,8,9,10)
+step=0
+while true
+do
+        array+=(${numbers[@]})
+        let step++
+        if [[ $step == 1000000 ]]; then
+                echo "${#array[@]}" >> report.log
+                step=0
+        fi
+done
---
+++ OS/LAB5/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+./m1.sh &
+./m2.sh &
+./m3.sh &
+cat .pid1
+cat .pid2
+cat .pid3
